82f44d899ff97dbd806e37e5275e5cbbe606532e
justin lee <lmnzzz@gmail.com>
2016-11-16 23:25:20 -0800

    list all searched profiles using recycler view base commit

---
+++ app/src/main/java/com/statstracker/something/ocst/Activities/ListProfilesActivity.java
@@ -0,0 +1,19 @@
+package com.statstracker.something.ocst.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.statstracker.something.ocst.R;
+
+/**
+ * Created by justin on 11/16/2016.
+ */
+
+public class ListProfilesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_profiles);
+    }
+}
